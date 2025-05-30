a95aeaed142d44b3cdbd1d14cca71abc2048b6c4
Your Name <justin.ypc@gmail.com>
2025-05-29 17:14:21 -0700

    added dummy data and fetchload

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
@@ -9,6 +16,33 @@ def check_mc():
         return jsonify({"valid": True})
     else:
         return jsonify({"valid": False})
+    
+@app.route('/fetchLoad', methods=['POST'])
+def fetchLoad():
+    data = request.get_json()
+    if not data or not "Location" in data or not "Minimum_Date" in data or not "Equipment" in data:
+        return jsonify({"error": True})
+    conn = mysql.connector.connect(
+        host="localhost",      # or your DB host
+        user="root",  # replace with your DB user
+        password="Happy",
+        database="loadDB"
+    )
+
+    cursor = conn.cursor(dictionary=True)
+    query = """
+        SELECT * FROM loads_template
+        WHERE origin LIKE %s
+        AND equipment_type LIKE %s
+        AND pickup_datetime > %s
+    """
+    params = (f"%{data["Location"]}%", f"%{data["Equipment"]}%", f"{data["Minimum_Date"]}")
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+    print(results)
+    cursor.close()
+    conn.close()
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=3001)
